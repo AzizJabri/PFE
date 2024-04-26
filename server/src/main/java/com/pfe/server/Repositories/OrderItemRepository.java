a54262e9f57647ab5df44581fdb1dd34695795e2
Aziz Jabri <79476299+AzizJabri@users.noreply.github.com>
2024-04-26 11:07:53 +0100

    fixed consistency issues

--- server/src/main/java/com/pfe/server/Repositories/OrderItemRepository.java
+++ server/src/main/java/com/pfe/server/Repositories/OrderItemRepository.java
@@ -4,19 +4,9 @@ import com.pfe.server.Models.OrderItems;
 import com.pfe.server.Models.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import org.springframework.data.redis.core.RedisHash;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-@Repository
-@RedisHash("OrderItems")
-
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 
