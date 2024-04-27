f65a7c868b854575d11dacb145877266a4cd53d3
Aziz Jabri <79476299+AzizJabri@users.noreply.github.com>
2024-04-27 13:22:43 +0100

    Testing Rolling updates k8s

--- server/src/main/java/com/pfe/server/Controllers/KubernetesController.java
+++ server/src/main/java/com/pfe/server/Controllers/KubernetesController.java
@@ -13,6 +13,6 @@ public class KubernetesController {
     private String hostname;
     @RequestMapping("/")
     public String index() {
-        return "Hello World!, Current HOSTNAME : " + hostname;
+        return "Hello World!, Current HOSTNAME : " + hostname + " (rolling update)";
     }
 }
