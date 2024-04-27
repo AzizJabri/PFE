202f23386c73937eea67625593b6584cfce6ac83
Aziz Jabri <79476299+AzizJabri@users.noreply.github.com>
2024-04-27 13:32:55 +0100

    added healthcheck endpoint

--- server/src/main/java/com/pfe/server/Controllers/KubernetesController.java
+++ server/src/main/java/com/pfe/server/Controllers/KubernetesController.java
@@ -1,6 +1,7 @@
 package com.pfe.server.Controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,10 @@ public class KubernetesController {
     @RequestMapping("/")
     public String index() {
         return "Hello World!, Current HOSTNAME : " + hostname + " (rolling update)";
+    }
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
     }
 }
